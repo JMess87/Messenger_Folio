c064a7bbaf5cfe37a3c69c21374cfb3b0548d0d1
Jennilee Messenger <jennilee.messenger@gmail.com>
2023-04-11 18:53:17 -0400

    updating starter code in this new repo, from practice activity

---
+++ src/components/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        My name is Jennilee Messenger. I am a full time Maximo Administrator for a major utility company. I specilize in . . . .
+        <ol>
+          <li> Innovation </li>
+          <li> Innovation </li>
+          <li> Innovation </li>
+          <li> Innovation </li>
+          <li> Innovation </li>
+          <li> Innovation </li>
+        </ol>
+      </p>
+    </div>
+  );
+}
---
+++ src/components/pages/Certs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Certs() {
+    return (
+        <div>
+            <h1>Certifications</h1>
+            <p>
+                Certifciations
+            </p>
+        </div>
+    );
+}
